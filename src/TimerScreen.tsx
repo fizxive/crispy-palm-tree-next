535b72f2af31cbf342503dc2452047ff61badd24
fizxive <imaginary.rhombus@gmail.com>
2021-01-13 14:11:59 +0000

    feat: impl timer

--- src/TimerScreen.tsx
+++ src/TimerScreen.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import PropTypes from "prop-types";
+import { Box, Typography } from "@material-ui/core";
 
-const TimerScreen = (props) => {
+const TimerScreen = () => {
+
+    const [time, setTime] = useState(100)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setTime((prev) => prev - 1)
+        }, 1000)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [time])
+
     return (
-        null
+        <Box>
+            <Typography>
+                {time}
+            </Typography>
+        </Box>
     )
 }
 
 TimerScreen.propType = {
-    
 }
 
 export default TimerScreen
