5d162c9f223239212b2fa76b595f96dd4133f52c
fizxive <imaginary.rhombus@gmail.com>
2021-01-13 13:57:26 +0000

    feat: add TimerScreen(FILE)

---
+++ src/TimerScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+const TimerScreen = (props) => {
+    return (
+        null
+    )
+}
+
+TimerScreen.propType = {
+    
+}
+
+export default TimerScreen
