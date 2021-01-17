9971bd4fab0d1a3fcdea60a8da3cac6f3f02eb0d
fizxive <imaginary.rhombus@gmail.com>
2021-01-17 10:26:15 +0000

    feat: add video.js usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import videojs from 'video.js';
+
 import './App.css';
+import 'video.js/dist/video-js.css'
+import logo from './logo.svg';
+
 import TimerScreen from './TimerScreen';
 
 function App() {
-  return (
-    <div className="App">
-      <TimerScreen />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const video = videojs(ref.current, {
+            controls: true,
+            controlBar: {
+                volumePanel: undefined,
+                fullscreenToggle: false
+            },
+            html5: {
+                hls: {
+                    overrideNative: false
+                },
+                nativeVideoTracks: true,
+                nativeAudioTracks: true,
+                nativeTextTracks: true
+            }
+        })
+        video.src("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")
+        // video.usingNativeControls(true)
+    }, [])
+
+    return (
+        <div className="App">
+            <video className="video-js" disablePictureInPicture playsInline controls ref={ref}>
+            </video>
+            <TimerScreen />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
         </a>
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
